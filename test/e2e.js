d622d5faa3e7581b354d022a02f18baad168162c
Thomas Hull <thull@scottlogic.co.uk>
2015-08-25 09:32:19 +0100

    Fixed made test pass for initial project

--- test/e2e.js
+++ test/e2e.js
@@ -3,101 +3,70 @@ var assert = require("chai").assert;
 var helpers = require("./e2eHelpers");
 
 testing.describe("end to end", function() {
+    var server;
+
     this.timeout(20000);
-    testing.before(helpers.setupDriver);
-    testing.beforeEach(helpers.setupServer);
-    testing.afterEach(helpers.teardownServer);
+
+    testing.beforeEach(function() {
+        server = helpers.setupServer();
+    });
+    testing.afterEach(function() {
+        helpers.teardownServer(server);
+    });
     testing.after(function() {
-        helpers.teardownDriver();
         helpers.reportCoverage();
     });
 
     testing.describe("on page load", function() {
         testing.it("displays TODO title", function() {
-            helpers.navigateToSite();
-            helpers.getTitleText().then(function(text) {
+            helpers.navigateToSite(server);
+            helpers.getTitleText(server).then(function(text) {
                 assert.equal(text, "TODO List");
             });
         });
         testing.it("displays empty TODO list", function() {
-            helpers.navigateToSite();
-            helpers.getTodoList().then(function(elements) {
+            helpers.navigateToSite(server);
+            helpers.getTodoList(server).then(function(elements) {
                 assert.equal(elements.length, 0);
             });
         });
         testing.it("displays an error if the request fails", function() {
-            helpers.setupErrorRoute("get", "/api/todo");
-            helpers.navigateToSite();
-            helpers.getErrorText().then(function(elements) {
-                assert.equal(elements.length, 1);
+            helpers.setupErrorRoute(server, "get", "/api/todo");
+            helpers.navigateToSite(server);
+            helpers.getErrorText(server).then(function(text) {
+                assert.equal(text, "Failed to get list. Server returned 500 - Internal Server Error");
             });
         });
     });
     testing.describe("on create todo item", function() {
         testing.it("clears the input field", function() {
-            helpers.navigateToSite();
-            helpers.addTodo("New todo item");
-            helpers.getInputText().then(function(value) {
+            helpers.navigateToSite(server);
+            helpers.addTodo(server, "New todo item");
+            helpers.getInputText(server).then(function(value) {
                 assert.equal(value, "");
             });
         });
         testing.it("adds the todo item to the list", function() {
-            helpers.navigateToSite();
-            helpers.addTodo("New todo item");
-            helpers.getTodoList().then(function(elements) {
+            helpers.navigateToSite(server);
+            helpers.addTodo(server, "New todo item");
+            helpers.getTodoList(server).then(function(elements) {
                 assert.equal(elements.length, 1);
             });
         });
         testing.it("displays an error if the request fails", function() {
-            helpers.setupErrorRoute("post", "/api/todo");
-            helpers.navigateToSite();
-            helpers.addTodo("New todo item");
-            helpers.getErrorText().then(function(elements) {
-                assert.equal(elements.length, 1);
+            helpers.setupErrorRoute(server, "post", "/api/todo");
+            helpers.navigateToSite(server);
+            helpers.addTodo(server, "New todo item");
+            helpers.getErrorText(server).then(function(text) {
+                assert.equal(text, "Failed to create item. Server returned 500 - Internal Server Error");
             });
         });
         testing.it("can be done multiple times", function() {
-            helpers.navigateToSite();
-            helpers.addTodo("New todo item");
-            helpers.addTodo("Another new todo item");
-            helpers.getTodoList().then(function(elements) {
+            helpers.navigateToSite(server);
+            helpers.addTodo(server, "New todo item");
+            helpers.addTodo(server, "Another new todo item");
+            helpers.getTodoList(server).then(function(elements) {
                 assert.equal(elements.length, 2);
-            });
-        });
-    });
-    testing.describe("on delete todo item", function() {
-        testing.it("Deletes an single item", function() {
-            helpers.navigateToSite();
-            helpers.addTodo("New todo item");
-            helpers.deleteTodo("0");
-            helpers.getTodoList().then(function(elements) {
-                assert.equal(elements.length, 0);
-            });
-        });
-
-        testing.it("Deletes an multiple item from list", function() {
-            helpers.navigateToSite();
-            helpers.addTodo("New todo item");
-            helpers.addTodo("nother todo item");
-            helpers.deleteTodo("1");
-            helpers.deleteTodo("0");
-            helpers.getTodoList().then(function(elements) {
-                assert.equal(elements.length, 0);
-            });
-        });
-    });
-    testing.describe("counting completed items", function() {
-        testing.it("Count is 0 when 0 items", function() {
-            helpers.navigateToSite();
-            helpers.getCountDiv().then(function(text) {
-                assert.equal(text, "Total ToDos left to do: 0");
-            });
-        });
-        testing.it("Counts an item", function() {
-            helpers.navigateToSite();
-            helpers.addTodo("New todo item");
-            helpers.getCountDiv().then(function(text) {
-                assert.equal(text, "Total ToDos left to do: 1");
             });
         });
     });
--- test/e2eHelpers.js
+++ test/e2eHelpers.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var createServer = require("../server/server");
+var server = require("../server/server");
 var webdriver = require("selenium-webdriver");
 var istanbul = require("istanbul");
 var path = require("path");
@@ -12,36 +12,27 @@ var collector = new istanbul.Collector();
 var gatheringCoverage = process.env.running_under_istanbul;
 var coverageFilename = "build_artifacts/coverage-e2e.json";
 
-var driver;
-var router;
-var server;
-
-module.exports.setupDriver = function() {
-    driver = new webdriver.Builder().forBrowser("chrome").build();
-};
-
-module.exports.setupServer = function(done) {
-    router = express.Router();
+module.exports.setupServer = function() {
+    var router = express.Router();
     if (gatheringCoverage) {
         router.get("/main.js", function(req, res) {
             var absPath = path.join(__dirname, "..", "public", "main.js");
             res.send(instrumenter.instrumentSync(fs.readFileSync("public/main.js", "utf8"), absPath));
         });
     }
-    server = createServer(testPort, router, done);
+    var serverInstance = server(testPort, router);
+    var driver = new webdriver.Builder().forBrowser("chrome").build();
+    return {router: router, server: serverInstance, driver: driver};
 };
 
-module.exports.teardownServer = function(done) {
-    server.close(done);
-};
-
-module.exports.teardownDriver = function() {
+module.exports.teardownServer = function(server) {
     if (gatheringCoverage) {
-        driver.executeScript("return __coverage__;").then(function (coverage) {
+        server.driver.executeScript("return __coverage__;").then(function (coverage) {
             collector.add(coverage);
         });
     }
-    driver.quit();
+    server.server.close();
+    server.driver.quit();
 };
 
 module.exports.reportCoverage = function() {
@@ -50,57 +41,43 @@ module.exports.reportCoverage = function() {
     }
 };
 
-module.exports.navigateToSite = function() {
-    driver.get(baseUrl);
+module.exports.navigateToSite = function(server) {
+    server.driver.get(baseUrl);
 };
 
-module.exports.getTitleText = function() {
-    return driver.findElement(webdriver.By.id("header")).getText();
+module.exports.getTitleText = function(server) {
+    return server.driver.findElement(webdriver.By.css("h1")).getText();
 };
 
-module.exports.getInputText = function() {
-    return driver.findElement(webdriver.By.id("new-todo")).getAttribute("value");
+module.exports.getInputText = function(server) {
+    return server.driver.findElement(webdriver.By.id("new-todo")).getAttribute("value");
 };
 
-module.exports.getErrorText = function() {
-    var errorElement = driver.findElement(webdriver.By.id("messages"));
-    driver.wait(webdriver.until.elementIsEnabled(errorElement), 5000);
-    return driver.findElements(webdriver.By.css("#messages div"));
+module.exports.getErrorText = function(server) {
+    var errorElement = server.driver.findElement(webdriver.By.id("error"));
+    server.driver.wait(webdriver.until.elementTextContains(errorElement, "Failed"), 5000);
+    return errorElement.getText();
 };
 
-module.exports.getTodoList = function() {
-    var todoListPlaceholder = driver.findElement(webdriver.By.id("todo-list-placeholder"));
-    driver.wait(webdriver.until.elementIsNotVisible(todoListPlaceholder), 5000);
-    return driver.findElements(webdriver.By.css("#todo-list li"));
+module.exports.getTodoList = function(server) {
+    var todoListPlaceholder = server.driver.findElement(webdriver.By.id("todo-list-placeholder"));
+    server.driver.wait(webdriver.until.elementIsNotVisible(todoListPlaceholder), 5000);
+    return server.driver.findElements(webdriver.By.css("#todo-list li"));
 };
 
-module.exports.addTodo = function(text) {
-    driver.findElement(webdriver.By.id("new-todo")).sendKeys(text);
-    driver.findElement(webdriver.By.id("submit-todo")).click();
+module.exports.addTodo = function(server, text) {
+    server.driver.findElement(webdriver.By.id("new-todo")).sendKeys(text);
+    server.driver.findElement(webdriver.By.id("submit-todo")).click();
 };
 
-module.exports.deleteTodo = function(index) {
-    var todoListPlaceholder = driver.findElement(webdriver.By.id("todo-list-placeholder"));
-    driver.wait(webdriver.until.elementIsNotVisible(todoListPlaceholder), 5000);
-    var button =  driver.findElement(webdriver.By.id("deleteTODO" + index));
-    button.click();
-};
-
-module.exports.getCountDiv = function(index) {
-    var todoListPlaceholder = driver.findElement(webdriver.By.id("todo-list-placeholder"));
-    driver.wait(webdriver.until.elementIsNotVisible(todoListPlaceholder), 5000);
-    var countDiv =  driver.findElement(webdriver.By.css("#count-label")).getText();
-    return countDiv;
-};
-
-module.exports.setupErrorRoute = function(action, route) {
+module.exports.setupErrorRoute = function(server, action, route) {
     if (action === "get") {
-        router.get(route, function(req, res) {
+        server.router.get(route, function(req, res) {
             res.sendStatus(500);
         });
     }
     if (action === "post") {
-        router.post(route, function(req, res) {
+        server.router.post(route, function(req, res) {
             res.sendStatus(500);
         });
     }
