2389630b8c5bca6753d955b9303dfa8863b4a53e
Thomas Hull <thull@scottlogic.co.uk>
2015-08-26 11:47:56 +0100

    Added todoservice for logic

---
+++ public/js/controllers/TodoController.js
@@ -0,0 +1,42 @@
+(function() {
+    angular.module("TodoApp").controller("TodoController", TodoController);
+
+    function TodoController($http, todoService) {
+        var vm = this;
+        $http.defaults.transformRequest.push(function(config) {
+            vm.loading = true;
+            return config;
+        });
+        $http.defaults.transformResponse.push(function(response) {
+            vm.loading = false;
+            return response;
+        });
+
+        vm.loading = false;
+        vm.items = [];
+
+        vm.submitForm = function() {
+            todoService.createTodo(vm.newTodo).then(reloadTodoList);
+            vm.newTodo = "";
+        };
+
+        vm.toggleTodoComplete = function(todo) {
+            todo.isComplete = !todo.isComplete
+            todoService.updateTodo(todo).then(reloadTodoList);
+        }
+
+        vm.deleteClick = function(todoId) {
+            todoService.deleteTodo(todoId).then(reloadTodoList);
+        }
+
+        function reloadTodoList() {
+            todoService.getTodoList().then(function(items) {
+                vm.items = items;
+            }, function(result) {
+                vm.error = "Failed to get list. Server returned " + result.status + " - " + result.data;
+            });
+        }
+
+        reloadTodoList();
+    };
+})();
---
+++ public/js/services/todoService.js
@@ -0,0 +1,45 @@
+(function() {
+    angular.module("TodoApp").factory('todoService', TodoService);
+
+    function TodoService($http){
+        var service = {
+            createTodo : createTodo,
+            getTodoList: getTodoList,
+            updateTodo: updateTodo,
+            deleteTodo: deleteTodo
+        };
+        return service
+
+        ///////////////////////
+
+        function createTodo(title) {
+            return $http.post("/api/todo", {
+                title: title
+            }).then(null, function(result) {
+                vm.error = "Failed to create item. Server returned " + result.status + " - " + result.data;
+            });
+        }
+
+        function getTodoList() {
+            return $http.get("/api/todo").then(function(result) {
+                return result.data;
+            });
+        }
+
+        function updateTodo(todo) {
+            return $http.put("/api/todo/" + todo.id, todo)
+            .then(null, function(result) {
+                vm.error = "Failed to update item. Server returned " + result.status + " - " + result.data;
+            });
+        }
+
+        function deleteTodo(todoId) {
+            return $http.delete("/api/todo/" + todoId)
+            .then(null, function(result) {
+                vm.error = "Failed to delte item. Server returned " + result.status + " - " + result.data;
+            });
+        }
+
+    }
+
+})();
