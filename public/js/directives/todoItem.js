c2b97eb1c149333fd8ff19b464de2240e4944624
Thomas Hull <thull@scottlogic.co.uk>
2015-08-28 15:55:48 +0100

    Added dbl-click to edit titles

--- public/js/directives/todoItem.js
+++ public/js/directives/todoItem.js
@@ -4,7 +4,7 @@
     function todoItem(){
         var directive = {
             scope: {
-                onDone: "=",
+                onUpdate: "=",
                 onDelete: "=",
                 item: "="
             },
@@ -14,15 +14,25 @@
         return directive;
 
         function linker(scope, element) {
-
+            scope.edit = false;
             scope.deleteTodo = function(){
                 scope.onDelete(scope.item.id);
             };
 
             scope.toggleTodoComplete = function() {
 
-                scope.onDone(scope.item);
+                scope.onUpdate(scope.item);
             };
+
+            scope.editTitle = function() {
+                scope.edit = true;
+            }
+
+            scope.submitForm = function() {
+                scope.edit = false;
+                scope.onUpdate(scope.item);
+
+            }
         }
     }
 })();
