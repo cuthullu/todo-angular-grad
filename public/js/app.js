ce294de99019ed8c82e774c17bc3fe0506722847
Thomas Hull <thull@scottlogic.co.uk>
2015-09-04 17:26:01 +0100

    Such CSS such Xperience

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
-angular.module("TodoApp", [ "ngStorage", "ngMaterial", "ngAnimate", "ui.sortable"]);
+angular.module("TodoApp", [ "ngStorage", "ngMaterial", "ngAnimate", "ui.sortable"]).config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme("default")
+        .primaryPalette("green")
+        .accentPalette("orange",{'default': '500', "hue-1":"600", "hue-2":"200", "hue-3":"A100"})
+        .warnPalette("blue-grey")
+        .backgroundPalette("grey",{'default': '50', "hue-1":"600", "hue-2":"200", "hue-3":"A100"});
+        //.backgroundPalette("grey").dark();
+});
